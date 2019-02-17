5f70275553bd83003930622bec2b056714a41d72
JungHyunKwon <kjh_3859@naver.com>
2019-02-18 02:23:03 +0900

    published

--- sprite.js
+++ sprite.js
@@ -9,7 +9,7 @@
 const fs = require('fs'),
 	  spriteSmith = require('spritesmith'), // {@link https://github.com/twolfson/spritesmith}
 	  baseDirectory = './images/sprite',
-	  imageExtensions = ['ase', 'art', 'bmp', 'blp', 'cd5', 'cit', 'cpt', 'cr2', 'cut', 'dds', 'dib', 'djvu', 'egt', 'exif', 'gif', 'gpl', 'grf', 'icns', 'ico', 'iff', 'jng', 'jpeg', 'jpg', 'jfif', 'jp2', 'jps', 'lbm', 'max', 'miff', 'mng', 'msp', 'nitf', 'ota', 'pbm', 'pc1', 'pc2', 'pc3', 'pcf', 'pcx', 'pdn', 'pgm', 'PI1', 'PI2', 'PI3', 'pict', 'pct', 'pnm', 'pns', 'ppm', 'psb', 'psd', 'pdd', 'psp', 'px', 'pxm', 'pxr', 'qfx', 'raw', 'rle', 'sct', 'sgi', 'rgb', 'int', 'bw', 'tga', 'tiff', 'tif', 'vtf', 'xbm', 'xcf', 'xpm', '3dv', 'amf', 'ai', 'awg', 'cgm', 'cdr', 'cmx', 'dxf', 'e2d', 'egt', 'eps', 'fs', 'gbr', 'odg', 'svg', 'stl', 'vrml', 'x3d', 'sxd', 'v2d', 'vnd', 'wmf', 'emf', 'art', 'xar', 'png', 'webp', 'jxr', 'hdp', 'wdp', 'cur', 'ecw', 'iff', 'lbm', 'liff', 'nrrd', 'pam', 'pcx', 'pgf', 'sgi', 'rgb', 'rgba', 'bw', 'int', 'inta', 'sid', 'ras', 'sun', 'tga'];
+	  imageExtensions = ['gif', 'png', 'jpg', 'jpeg'];
 
 /**
  * @name 숫자 비교
@@ -159,7 +159,7 @@ fs.readdir(baseDirectory, (err, directories) => {
 										let file = files[filesIndex],
 											fileDirectory = directory + '/' + file,
 											fileSplit = file.split('.'),
-											fileExtensions = fileSplit[1];
+											fileExtensions = fileSplit[fileSplit.length - 1];
 										
 										//문자일 때
 										if(typeof fileExtensions === 'string') {
